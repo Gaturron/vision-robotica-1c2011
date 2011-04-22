eaec28e12883013cff9c5f6c224d544c22d048e6
fernando.bugni <fernando.bugni@7e587de1-f32e-df48-6301-0d8793c08fda>
2011-04-22 20:31:04 +0000

    agrego el ej2 y una nueva version del ej 1 Los dos fallan en lo mismo. Rectifican como el ogt

--- trunk/TP1/rectificacion1.m
+++ trunk/TP1/rectificacion1.m
@@ -1,4 +1,4 @@
-function p = rectificacion(x)
+function p = rectificacion1(x)
     image = imread(x);
     [tamx, tamy] = size(image);
 
@@ -36,13 +36,32 @@ function p = rectificacion(x)
     %si, los nombres son una bostaaa :P
    
     %+Calcular P inf 1
-    p_prima = [ ((a_prima * (a_prima + b_prima))/(a_prima - b_prima)) 1];
-    pendiente_1 = (p3(1)-p1(1))/(p3(2)-p1(2));
-    p_inf1 = pendiente_1 * p_prima + p1
+    
+    %calcular P inf 1 forma vieja
+    %p_prima = [ ((a_prima * (a_prima + b_prima))/(a_prima - b_prima)) 1];
+    %pendiente_1 = ((p3(1)-p1(1))/(p3(2)-p1(2)));
+    %p_inf1 = pendiente_1 * p_prima + p1
+    
+    %calcular P inf 1 forma nueva (no anda ninguna igual)
+    pendiente_1 = ((p3(1)-p1(1))/(p3(2)-p1(2)));
+    ang = atan(pendiente_1);
+    op = sin(ang) * ((a_prima * (a_prima + b_prima))/(a_prima - b_prima));
+    ady = cos(ang) * ((a_prima * (a_prima + b_prima))/(a_prima - b_prima));
+    p_inf1 = [op ady] + p1
+    
     %+Calcular P inf 2
-    p_prima_prima = [ ((a_prima_prima * (a_prima_prima + b_prima_prima))/(a_prima_prima - b_prima_prima)) 1];
-    pendiente_2 = (p6(1)-p4(1))/(p6(2)-p4(2));
-    p_inf2 = pendiente_2 * p_prima_prima + p4
+    
+    %calcular P inf 2 forma vieja
+    %p_prima_prima = [ ((a_prima_prima * (a_prima_prima + b_prima_prima))/(a_prima_prima - b_prima_prima)) 1];
+    %pendiente_2 = ((p6(1)-p4(1))/(p6(2)-p4(2)));
+    %p_inf2 = pendiente_2 * p_prima_prima + p4
+    
+    %calcular P inf 2 forma nueva (no anda ninguna igual)
+    pendiente_2 = ((p6(1)-p4(1))/(p6(2)-p4(2)));
+    ang = atan(pendiente_2);
+    op = sin(ang) * ((a_prima_prima * (a_prima_prima + b_prima_prima))/(a_prima_prima - b_prima_prima));
+    ady = cos(ang) * ((a_prima_prima * (a_prima_prima + b_prima_prima))/(a_prima_prima - b_prima_prima));
+    p_inf2 = [op ady] + p4
 
     %+Cross de P inf 1 y P inf 2
     L = cross([p_inf1 1], [p_inf2 1])
@@ -50,17 +69,19 @@ function p = rectificacion(x)
         
     %+Tenemos L: hallar H
     H = [1 0 0 ; 0 1 0; L]
-
+    %H = [1 0 0; 0 1 0; 0 0 1];
+    
     %+For que llene la imagen
-    image_rec = zeros(tamx*3, tamy*3);
+    image_rec = zeros(tamx*2, tamy*2);
     image_rec = uint8(image_rec);
     size(image_rec)
-    for x = 1:1:tamx*3
-        for y = 1:1:tamy*3
+    for x = 1:1:tamx*2
+        for y = 1:1:tamy*2
             temp = H * [x; y; 1];
-            temp = round(temp/temp(3)) ;
+            temp = (round(temp/temp(3)));
             if(1 <= temp(1) && temp(1) <=tamx && 1 <= temp(2) && temp(2) <=tamy)
-                image_rec(x, y) = image(temp(1),temp(2));
+                image_rec((x+tamx/4), (y+tamy/4)) = image(temp(1),temp(2));
+                10
             end
         end
     end
