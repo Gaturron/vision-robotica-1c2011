b427d3880058549b919a4bb2501182a9ab36c984
amitstein <amitstein@7e587de1-f32e-df48-6301-0d8793c08fda>
2011-06-23 22:50:44 +0000

    Primer intento

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -26,20 +26,49 @@ int main(int argc, char *argv[]) {
     
     cv::FileNode root = fs.root();
     cv::Mat p1,p2, dist1, dist2, r, t;
-    
+
     root["P1"] >> p1;
     root["P2"] >> p2;
     root["dist1"] >> dist1;
     root["dist2"] >> dist2;
     root["R"] >> r;
     root["T"] >> t;
+
+    cv::Mat img1, img2, img1_rect, img2_rect;   
+   //imagenes
+    img1 = cv::imread("Image1_left.tif",1);
+    img2 = cv::imread("Image1_right.tif",1);
+    cv::Size size = img1.size();
+//  size = cv::Size(img1.width, img1->height);
+
+//Matrices ya rectificadas
+   cv::Mat P1, P2, R1, R2, Q;
+   double alpha = 0.0;
+   cv::Rect roi1, roi2;
    
+   cv::stereoRectify(p1, dist1, p2, dist2, size, r, t, R1, R2, P1, P2, Q, alpha, size, &roi1, &roi2, cv::CALIB_ZERO_DISPARITY); 
+//void stereoRectify(const Mat&  cameraMatrix1, const Mat&  distCoeffs1, const Mat&  cameraMatrix2, const Mat&  distCoeffs2, Size imageSize, const Mat&  R, const Mat&  T, Mat&  R1, Mat&  R2, Mat&  P1, Mat&  P2, Mat&  Q, double alpha, Size newImageSize=Size(), Rect*  roi1=0, Rect* roi2=0, int flags=CALIB_ZERO_DISPARITY)
+
+  cv::Mat mapx1, mapy1, mapx2, mapy2;
+  cv::initUndistortRectifyMap(p1, dist1, R1, P1, size, CV_32FC1, mapx1, mapy1);
+  cv::initUndistortRectifyMap(p2, dist2, R2, P2, size, CV_32FC1, mapx2, mapy2);
+  //void initUndistortRectifyMap(const Mat&  cameraMatrix, const Mat&  distCoeffs, const Mat&  R, const Mat&  newCameraMatrix, Size size, int m1type, Mat&  map1, Mat&  map2)¶
+  
+  
+  cv::remap(img1, img1_rect, mapx1, mapy1, cv::INTER_LINEAR);
+  cv::remap(img2, img2_rect, mapx2, mapy2, cv::INTER_LINEAR);
+  //void remap(const Mat&  src, Mat&  dst, const Mat&  map1, const Mat&  map2, int interpolation, int borderMode=BORDER_CONSTANT, const Scalar& borderValue=Scalar())¶
+  
+  cv::namedWindow("imagen", CV_WINDOW_AUTOSIZE);
+  cv::imshow("imagen",img1_rect);
+  cv::waitKey(0);
+  
+   //cv::MatIterator_<double> myIter = P1.begin<double>(), it_end = P1.end<double>();
    
-   cv::MatIterator_<double> myIter = p1.begin<double>(), it_end = p1.end<double>();
-   
-   for (; myIter!=it_end; ++myIter){
-    double v = *myIter;
-    cout << v << endl;
-   } 
+  // for (; myIter!=it_end; ++myIter){
+  //  double v = *myIter;
+  //  cout << v << endl;
+  // } 
+  
    return 0;
 }
